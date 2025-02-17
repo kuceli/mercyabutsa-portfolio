5697c8fcaa9986c6d213249d5e9adc5b74bd661e
Englama Kuceli <englamakuceli@Kucelis-MBP.fios-router.home>
2025-02-17 16:20:20 -0500

    reworked header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import { FaDownload } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <nav className="text-[#07363C] py-3 px-8 w-[80%] mx-auto mt-6 rounded-full flex items-center justify-between shadow-[0_3px_10px_rgb(0,0,0,0.2)] fixed top-0 left-0 right-0 bg-white">
+    <nav className="text-[#07363C] text-base font-medium py-3 px-20 w-full mx-auto flex items-center justify-between fixed top-0 left-0 right-0 bg-white">
       <img src={Logo} alt="logo" className="w-[6%] h-[6%]" />
-      <ul className="flex space-x-6 uppercase font-medium justify-center  items-center">
+      <ul className="flex space-x-10 justify-center items-center">
         <li>
           <Link to="/" className="hover:text-[#F48D3F]">
             Home
@@ -28,16 +28,16 @@ const Header = () => {
             Contact
           </Link>
         </li>
-        <li className="text-[#F48D3F] flex items-center space-x-2 border border-solid border-[#F48D3F] px-4 py-3 rounded-full">
-          <a
-            href="https://drive.google.com/file/d/1uVv4afeJ_GSY67pm_SSGUFGeznKfyx8d/view?usp=sharing"
-            target="_blank"
-          >
-            Download CV
-          </a>
-          <FaDownload size={20} />
-        </li>
       </ul>
+      <div className="text-[#F48D3F] flex items-center space-x-2 border border-solid border-[#F48D3F] px-4 py-3 rounded-full">
+        <a
+          href="https://drive.google.com/file/d/1uVv4afeJ_GSY67pm_SSGUFGeznKfyx8d/view?usp=sharing"
+          target="_blank"
+        >
+          Download CV
+        </a>
+        <FaDownload size={20} />
+      </div>
     </nav>
   );
 };
