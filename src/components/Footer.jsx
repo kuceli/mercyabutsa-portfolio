42f0723048cebe53ccee9dc8dcff3a29a69107fa
Englama Kuceli <englamakuceli@Kucelis-MBP.fios-router.home>
2025-07-01 16:32:33 -0400

    fixed responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 const Footer = () => {
   return (
-    // <div className="hidden md:flex items-center md:px-12 lg:px-20 h-[48px] w-full text-white bg-[#07363C] mt-6">
-    //   Copyright © 2025
-    // </div>
-    <div className="flex items-center justify-center px-6 md:px-12 lg:px-20 h-[48px] w-full text-white bg-[#07363C]">
+    <div className="hidden md:flex items-center md:px-12 lg:px-20 h-[48px] w-full text-white bg-[#07363C]">
       Copyright © 2025
     </div>
   );
